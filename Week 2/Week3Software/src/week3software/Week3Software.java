2a3d3e73d75e309e75bba379dabae2675eb78b8a
mensahna076 <mensahna@sheridancollege.ca>
2020-06-06 17:33:48 -0400

    3rd commit

---
+++ Week 2/Week3Software/src/week3software/Week3Software.java
@@ -0,0 +1,15 @@
+
+package week3software;
+
+
+public class Week3Software {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Week 3 software design example
+        System.out.println("example week 3");
+    }
+    
+}
